34daac176ba348c88c4eb630ea772d755263c5bc
Cristian Arce Jimenez <cristian.arce@Cristian-Arce-MacBook-Pro.local>
2021-05-07 13:52:36 -0600

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'normalize.css'
+import '@/assets/styles/index.css'
+import router from './router'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
+
+
+
+// import Vue from 'vue'
+// import 'normalize.css'
+// import '@/assets/styles/index.css'
+
+// import App from './App.vue'
+// import router from './router'
+
+// Vue.config.productionTip = false
+
+// new Vue({
+//   router,
+//   render: h => h(App),
+// }).$mount('#app')
